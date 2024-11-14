8cfe80b8591a9331cf0b81d7cd8c02e7a90d369c
siwol025 <cyk99115695@gmail.com>
2024-11-14 15:33:18 +0900

    BoardRepository 생성

---
+++ src/main/java/com/siwol/board/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.siwol.board.repository;
+
+import com.siwol.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
