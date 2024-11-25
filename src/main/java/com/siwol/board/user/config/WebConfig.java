8cf9a3beb7cd7535ed38d8ac9a0358c06a812f69
siwol025 <cyk99115695@gmail.com>
2024-11-25 13:42:35 +0900

    refactor: Board 관련 클래스명을 Post로 변경

--- src/main/java/com/siwol/board/user/config/WebConfig.java
+++ src/main/java/com/siwol/board/user/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.siwol.board.user.config;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
